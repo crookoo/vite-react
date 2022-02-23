1ae0be157c4fb960f6a45021303486e83341e40c
Michael Zimmermann <info@michaelzimmermann.com>
2022-02-23 16:41:42 +0100

    Adjust vite.config.ts: postcss plugin

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,25 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-  // css: { preprocessorOptions: { scss: { charset: false } } },
+  plugins: [react()],
   // build: {
   //   cssCodeSplit: true,
   //   sourcemap: true,
   // }
+  css: {
+    postcss: {
+      plugins: [
+        {
+          postcssPlugin: 'internal:charset-removal',
+          AtRule: {
+            charset: (atRule) => {
+              if (atRule.name === 'charset') {
+                atRule.remove();
+              }
+            }
+          }
+        }
+      ]
+    }
+  }
 })
