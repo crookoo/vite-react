21a66fc1d0cbf5b5bd91dd9362d6d7a515227244
Michael Zimmermann <info@michaelzimmermann.com>
2022-03-19 16:38:47 +0100

    CSS: Styling h1/h2 PageScreen

--- src/components/PageScreen.tsx
+++ src/components/PageScreen.tsx
@@ -12,7 +12,7 @@ export default function PageScreen(props: Props): JSX.Element {
 
     if (page) {
         return (
-            <div className="container pt-6 pb-4">
+            <div className="container pt-6 pb-4 page-screen">
                 <div className="row">
                     <div className="col-xl-8 offset-xl-2">
                         <MetaDecorator title={page.metaTitle} description={page.metaDescription} />
