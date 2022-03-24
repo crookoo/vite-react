bf865bbda994098c7ab1254060834f511fc958e9
Michael Zimmermann <info@michaelzimmermann.com>
2022-03-24 09:58:02 +0100

    Favicon + Navbar-Logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom';
+import logo from '../assets/favicon.svg';
 
 export default function Navbar() {
     let navigate = useNavigate();
@@ -9,7 +10,10 @@ export default function Navbar() {
                 <div className="row">
                     <div className="col-xl-8 offset-xl-2 ">
                         <div className="d-flex justify-content-between">
-                            <Link className="navbar-brand" to="/">stage4</Link>
+                            <Link className="navbar-brand" to="/">
+                                <img src={logo} alt="Logo" width="26" height="26" className="d-inline-block align-text-top me-3" />
+                                stage4
+                            </Link>
                             <div onClick={() => navigate(-1)} className="circle">‹</div>
                         </div>
                     </div>
